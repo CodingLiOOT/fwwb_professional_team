b1aaa4738d7e409bd03fe14931bc3f638a77935f
wdi-nancy <67570567+wdi-nancy@users.noreply.github.com>
2021-01-27 17:20:37 +0800

    搭建element-ui框架，插入数字翻牌器，排名轮播图（有问题待解决）

---
+++ fwwb_front_end/src/components/data_V.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <el-row :gutter="10">
+      <el-col :span="5">
+        <el-select v-model="value" filterable placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-col>
+      <el-col :span=11>
+        <el-time-picker
+          v-model="value1"
+          :picker-options="{
+      selectableRange: '18:30:00 - 20:30:00'
+    }"
+          placeholder="任意时间点">
+        </el-time-picker>
+        <el-time-picker
+          arrow-control
+          v-model="value2"
+          :picker-options="{
+      selectableRange: '18:30:00 - 20:30:00'
+    }"
+          placeholder="任意时间点">
+        </el-time-picker>
+      </el-col>
+      <el-col :span="8">
+        <dv-digital-flop :config="config" style="width:200px;height:50px;" />
+      </el-col>
+    </el-row>
+    <el-row :gutter="10">
+      <el-col :span="16">
+        <dv-digital-flop :config="config_rank" style="width:100px;height:50px;" />
+      </el-col>
+      <el-col :span="8">
+        <h1>进度池</h1>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "dataV",
+
+      data() {
+        return {
+          config : {
+            number: [100],
+            content: '{nt}个'
+          },
+          config_rank:{
+           data: [
+              {
+                name: '周口',
+                value: 55
+              },
+              {
+                name: '南阳',
+                value: 120
+              },
+              {
+                name: '西峡',
+                value: 78
+              },
+              {
+                name: '驻马店',
+                value: 66
+              },
+              {
+                name: '新乡',
+                value: 80
+              },
+              {
+                name: '信阳',
+                value: 45
+              },
+              {
+                name: '漯河',
+                value: 29
+              }
+            ]
+          },
+          value1: new Date(2016, 9, 10, 18, 40),
+          value2: new Date(2016, 9, 10, 18, 40),
+          options: [{
+            value: '选项1',
+            label: '1号线'
+          }, {
+            value: '选项2',
+            label: '2号线'
+          }, {
+            value: '选项3',
+            label: '4号线'
+          }, {
+            value: '选项4',
+            label: '6号线'
+          }, {
+            value: '选项5',
+            label: '9号线'
+          }],
+          value: ''
+
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  el-row {
+    margin-bottom: 20px;
+  /*&:last-child {*/
+  /*   margin-bottom: 0;*/
+  /* }*/
+  }
+  el-col {
+    border-radius: 4px;
+  }
+
+</style>
