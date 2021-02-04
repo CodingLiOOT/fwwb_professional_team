0fcaa6d7a70034d9f636ca86ca1f840194acae8d
HarryZhou-618 <59814103+HarryZhou-618@users.noreply.github.com>
2021-02-04 12:25:47 +0800

    Add Station implement

---
+++ fwwb_back_end/src/main/java/com/fwwb/back_end/controller/StationController.java
@@ -0,0 +1,16 @@
+package com.fwwb.back_end.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author： Harry Zhou
+ * @description: Station Controller
+ * @date: Created in 11:29 2021/2/4
+ */
+
+@RestController
+@RequestMapping(value = "/api/station")
+public class StationController {
+
+}
---
+++ fwwb_back_end/src/main/java/com/fwwb/back_end/dao/StationMapper.java
@@ -0,0 +1,30 @@
+package com.fwwb.back_end.dao;
+
+import org.apache.ibatis.annotations.MapKey;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author： Harry Zhou
+ * @description: Station Mapper
+ * @date: Created in 11:30 2021/2/4
+ */
+
+@Mapper
+@Repository
+public interface StationMapper {
+    @MapKey("in_hour")
+    @Select("select " +
+            "date_format(in_time,'%Y-%m-%d %H:00:00') in_hour," +
+            "count(userID) " +
+            "from trips " +
+            "where in_time between '2020-02-02 12:00:00' " +
+            "and '2020-02-02 16:00:00' " +
+            "group by in_hour " +
+            "order by in_time;")
+    List<HashMap<String,Integer>> getPassenger();
+}
