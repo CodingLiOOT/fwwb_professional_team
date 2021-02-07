f4a6c34c26f625a11be0d95b9f773edec694538c
wdi-nancy <67570567+wdi-nancy@users.noreply.github.com>
2021-02-07 16:31:20 +0800

    x轴待调整

---
+++ MainPage.vue
@@ -0,0 +1,115 @@
+<template>
+  <el-container style="width: 100%;height: 100%; top: 0; bottom: 0;" direction="vertical">
+    <el-header class="myHeader" style="padding: 0">
+      <el-row>
+        <el-col :span="8"><img class="myImg" src="../assets/logo.png"></el-col>
+        <el-col :span="8" align="middle">
+          <div class="myTitle">
+            <strong style = "color:#ffffff">专业团队</strong>
+          </div>
+        </el-col>
+        <el-col :span="8" class="username"></el-col>
+      </el-row>
+    </el-header>
+    <el-container direction="horizontal" style=" height: 100%;width: 100%; top: 0; bottom: 0;">
+      <el-aside width="200px">
+        <el-row class="tac">
+          <el-col :span="12">
+            <el-menu
+              default-active="2"
+              class="el-menu-vertical-demo"
+              router
+              @open="handleOpen"
+              @close="handleClose">
+              <el-menu-item index="/mainPage/data_V">
+                <i class="el-icon-menu"></i>
+                <template slot="title">总览和线</template>
+              </el-menu-item>
+              <el-menu-item index="/mainPage/echarts">
+                <i class="el-icon-document"></i>
+                <span slot="title">站点</span>
+              </el-menu-item>
+              <el-menu-item index="/mainPage/baiduMap">
+                <i class="el-icon-document"></i>
+                <span slot="title">baidu-map</span>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+
+
+  export default {
+    name: "MainPage",
+
+    data() {
+      return {
+        fold: 'el-icon-s-unfold',
+        unfold: 'el-icon-s-fold',
+        myIcon: 'el-icon-s-fold',
+        isFold: true,
+      }
+    },
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+
+    }
+  }
+</script>
+<style>
+  .tac {
+    margin: 0;
+    height: 100%;
+  }
+
+  html, body {
+    margin: 0;
+    top: 0;
+    bottom: 0;
+    height: 100%;
+  }
+
+  .myHeader {
+    background-color: #60b4c9;
+  }
+
+  .myImg {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    -webkit-border-radius: 50%;
+    -moz-border-radius: 50%;
+    margin: 5px;
+  }
+
+  .myTitle {
+    font-family: SimSun;
+    font-size: 30px;
+    height: 60px;
+    line-height: 60px;
+  }
+
+  .el-menu-mainBar i {
+    color: #ffffff;
+  }
+  .username{
+    margin-top: 20px;
+    font-size: 20px;
+    color: white;
+  }
+
+
+</style>
