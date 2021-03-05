6739b6902fe75dbc3691adc18bd81dd1fe519c62
nancy_wdi <nancy_wdi@163.com>
2021-03-05 12:03:58 +0800

    overview version of wd

--- fwwb_front_end/src/components/Overview.vue
+++ fwwb_front_end/src/components/Overview.vue
@@ -2,12 +2,12 @@
   <div>
     <el-row :gutter="5">
       <el-col :span="5">
-        <div class="fs-28">{{ date.hh + ':'+ date.MM + ':'+ date.ss }}</div>
-        <div class="fs-14">{{ date.yyyy + '-'+ date.mm + '-'+ date.dd }}</div>
-        <div class="left fs-18 lh-42">星期{{ date.week }}</div>
+        <h2 class="nowTime">当前时间</h2>
+        <div class="nowTime">{{nowTime.date}}</div>
+        <div class="nowTime">{{nowTime.time}}</div>
       </el-col>
       <el-col :span="7">
-<!--        天气插件-->
+        <div id="weather-v2-plugin-standard"></div>
       </el-col>
       <el-col :span=5>
 <!--        人数-->
@@ -16,9 +16,23 @@
 <!--        水位图-->
       </el-col>
     </el-row>
+    <el-row :gutter="10">
+      <el-col :offset="0" :span="6">
+        <h1 style=color:#3fdcdc;>当天总人次：</h1>
+      </el-col>
+      <el-col :offset="0" :span="6">
+        <h1 style=color:#3fdcdc;>热力拓扑图：</h1>
+      </el-col>
+      <el-col :offset="0" :span="6">
+        <h1 style=color:#3fdcdc;>线路占比：</h1>
+      </el-col>
+      <el-col :offset="0" :span="6">
+        <h1 style=color:#3fdcdc;>入站站点排行表：</h1>
+      </el-col>
+    </el-row>
     <el-row :gutter="5">
       <el-col :span="5">
-<!--        当天折线图-->
+        <div id="dayChart" :style="{width: '25rem', height: '25rem'}"></div>
       </el-col>
       <el-col :span="5">
         <!--        热力拓扑图-->
@@ -30,9 +44,23 @@
         <!--        前十入站轮播表-->
       </el-col>
     </el-row>
+    <el-row :gutter="10">
+      <el-col :offset="0" :span="6">
+        <h1 style=color:#3fdcdc;>未来七天总人次：</h1>
+      </el-col>
+      <el-col :offset="0" :span="6">
+        <h1 style=color:#3fdcdc;>热力OD图：</h1>
+      </el-col>
+      <el-col :offset="0" :span="6">
+        <h1 style=color:#3fdcdc;>年龄结构：</h1>
+      </el-col>
+      <el-col :offset="0" :span="6">
+        <h1 style=color:#3fdcdc;>出站站点排行表：</h1>
+      </el-col>
+    </el-row>
     <el-row :gutter="5">
       <el-col :span="5">
-        <!--        未来七天折线图-->
+        <div id="weekChart" :style="{width: '25rem', height: '25rem'}"></div>
       </el-col>
       <el-col :span="5">
         <!--        OD热力图-->
@@ -52,53 +80,173 @@ export default {
 name: "Overview",
   data() {
     return {
-      date:{
-        yyyy:'',
-        mm:'',
-        dd:'',
-        hh:'',
-        MM:'',
-        ss:'',
-        week:'',
-      }
+      nowTime:{
+        date:'',
+        time:'',
+      },
     };
   },
   methods: {
-    getClientTime() {
-      const date = new Date()
-      const weeks = ['天', '一', '二', '三', '四', '五', '六']
-      const that = this
-      this.date = {
-        yyyy: date.getFullYear(),
-        mm: date.getMonth() + 1,
-        dd: date.getDate(),
-        hh: date.getHours(),
-        MM: date.getMinutes(),
-        ss: date.getSeconds(),
-        week: weeks[date.getDay()]
-      }
-      if (this.date.mm < 10) {
-        this.date.mm = '0' + this.date.mm
-      }
-      if (this.date.dd < 10) {
-        this.date.dd = '0' + this.date.dd
-      }
-      if (this.date.hh < 10) {
-        this.date.hh = '0' + this.date.hh
-      }
-      if (this.date.MM < 10) {
-        this.date.MM = '0' + this.date.MM
-      }
-      if (this.date.ss < 10) {
-        this.date.ss = '0' + this.date.ss
-      }
+    timeFormat(timeStamp) {
+      let year = new Date(timeStamp).getFullYear();
+      let month =new Date(timeStamp).getMonth() + 1 < 10? "0" + (new Date(timeStamp).getMonth() + 1): new Date(timeStamp).getMonth() + 1;
+      let date =new Date(timeStamp).getDate() < 10? "0" + new Date(timeStamp).getDate(): new Date(timeStamp).getDate();
+      let hh =new Date(timeStamp).getHours() < 10? "0" + new Date(timeStamp).getHours(): new Date(timeStamp).getHours();
+      let mm =new Date(timeStamp).getMinutes() < 10? "0" + new Date(timeStamp).getMinutes(): new Date(timeStamp).getMinutes();
+      let ss =new Date(timeStamp).getSeconds() < 10? "0" + new Date(timeStamp).getSeconds(): new Date(timeStamp).getSeconds();
+      this.nowTime.date = year + "年" + month + "月" + date +"日";
+      this.nowTime.time=hh+":"+mm+':'+ss
+    },
+    nowTimes(){
+      this.timeFormat(new Date());
+      setInterval(this.nowTimes,1000);
+      this.clear()
+    },
+    clear(){
+      clearInterval(this.nowTimes)
+      this.nowTimes = null;
+    },
+    dayChartInit(){
+      let inChart = this.$echarts.init(document.getElementById('dayChart'))
+      inChart.setOption({
+        title:{
+          text: '当天总人次',
+          bottom:0,
+          left:'center'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            crossStyle: {
+              color: '#999'
+            }
+          }
+        },
+        toolbox: {
+          feature: {
+            dataView: {show: true, readOnly: false},
+            magicType: {show: true, type: ['line', 'bar']},
+            restore: {show: true},
+            saveAsImage: {show: true}
+          }
+        },
+        legend: {
+          data: ['人次'],
+          width:300,
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+            axisPointer: {
+              type: 'shadow'
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value',
+            // min: 0,
+            // max: 250,
+            // interval: 50,
+            // axisLabel: {
+            //   formatter: '{value} ml'
+            // }
+          }
+        ],
+        series: [
+          {
+            name: '人次',
+            type: 'line',
+            data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+          }
+        ]
+      });
+    },
+    weekChartInit(){
+      let inChart = this.$echarts.init(document.getElementById('weekChart'))
+      inChart.setOption({
+        title:{
+          text: '当天总人次',
+          bottom:0,
+          left:'center'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            crossStyle: {
+              color: '#999'
+            }
+          }
+        },
+        toolbox: {
+          feature: {
+            dataView: {show: true, readOnly: false},
+            magicType: {show: true, type: ['line', 'bar']},
+            restore: {show: true},
+            saveAsImage: {show: true}
+          }
+        },
+        legend: {
+          data: ['人次'],
+          width:300,
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+            axisPointer: {
+              type: 'shadow'
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value',
+            // min: 0,
+            // max: 250,
+            // interval: 50,
+            // axisLabel: {
+            //   formatter: '{value} ml'
+            // }
+          }
+        ],
+        series: [
+          {
+            name: '人次',
+            type: 'line',
+            data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+          }
+        ]
+      });
     },
   },
   mounted() {
     // 获取当前时间
-    setTimeout(() => {
-      this.getClientTime()
-    }, 1000)
+    this.nowTimes();
+    // 天气
+    window.WIDGET = {
+      CONFIG: {
+        "layout": 1,
+        "width": "450",
+        "height": "150",
+        "background": 1,
+        "dataColor": "FFFFFF",
+        "language": "zh",
+        "borderRadius": "10",
+        "city": "CN101010100",
+        "key": "kAccMmzpUi"
+      }
+    };
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://apip.weatherdt.com/standard/static/js/weather-standard-common.js?v=2.0';
+    document.getElementsByTagName('head')[0].appendChild(script);
+    // 画图
+    this.dayChartInit();
+    this.weekChartInit();
     // 设置背景
     document.querySelector('body').setAttribute('style', 'background-color:#16191D')
   },
@@ -112,5 +260,8 @@ name: "Overview",
 </script>
 
 <style scoped>
-
+  .nowTime{
+    color: white;
+    font-size: 25px;
+  }
 </style>
