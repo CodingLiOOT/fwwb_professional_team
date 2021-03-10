e80b0eff7046eec6898270784eecdecbc2aa76c9
CodeingLiOOT <296277916@qq.com>
2021-03-10 19:49:02 +0800

    整合日志框架log4j2

--- fwwb_back_end/src/main/java/com/fwwb/back_end/utils/token/JWTUtils.java
+++ fwwb_back_end/src/main/java/com/fwwb/back_end/utils/token/JWTUtils.java
@@ -5,6 +5,8 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fwwb.back_end.entity.AccountBean;
+import com.fwwb.back_end.utils.exceptionHandler.exception.DefinitionException;
+import com.fwwb.back_end.utils.exceptionHandler.exception.ErrorEnum;
 
 
 import java.util.Date;
@@ -50,5 +52,18 @@ public class JWTUtils {
         }catch (Exception e){
             return false;
         }
+    }
+
+    public static String verifyAndGetUserName(String token,String claim){
+         try {
+             DecodedJWT jwt= JWT.require(Algorithm.HMAC256(TOKEN_SECRET))
+                     .withIssuer("CodeingLiOOT")
+                     .build().verify(token);
+            return jwt.getClaim(claim).asString();
+         }catch (Exception e){
+             throw new DefinitionException(ErrorEnum.SIGNATURE_NOT_MATCH);
+         }
+
+
     }
 }
