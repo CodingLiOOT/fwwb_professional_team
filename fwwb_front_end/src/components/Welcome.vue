8a2c8fc502b0e88c1c9b666b382901401dcfa739
Lipyu <65934928+Lipyu@users.noreply.github.com>
2021-04-21 11:06:15 +0800

    靓靓登录界面

--- fwwb_front_end/src/components/Welcome.vue
+++ fwwb_front_end/src/components/Welcome.vue
@@ -1,337 +1,270 @@
 <template>
-  <div id="welcome">
-    <div class="control">
-      <div class="item">
-        <div class="active">Sign in</div><div>Sign up</div>
-      </div>
-      <div class="content">
-        <form action="http://localhost:8080/login" method="post" id="login_form">
-          <table>
-            <div style="display: block;">
-              <p>Account</p>
-              <input type="text" id="login_username_input" name="login_username_input" placeholder="username" style="width: 20rem"/>
-              <span id="login_username_error" class="error"></span>
-              <p>Password</p>
-              <input type="password" id="login_password_input" name="login_password_input" placeholder="password" />
-              <span id="login_password_error" class="error"></span>
-              <br/><br/>
-              <label><input type="checkbox" id="remeberyhm">记住密码</label>
-              <br/>
-              <input id="login_submit" type="submit" value="Login" />
-            </div>
-          </table>
-        </form>
-
-        <form action="http://localhost:8080/register" method="post" id="logon_form">
-          <div>
-            <p>Account</p>
-            <input type="text" id="logon_username_input" name="logon_username_input" placeholder="username" />
-            <span id="logon_username_error" class="error"></span>
-            <p>Password</p>
-            <input type="password" id="logon_password_input" name="logon_password_input" placeholder="password" />
-            <span id="logon_password_error" class="error"></span>
-            <br/>
-            <input id="logon_submit" type="submit" value="Logon" />
-          </div>
-        </form>
-      </div>
-    </div>
+  <div class="homepage-hero-module">
+    <div :style="fixStyle" class="filter"></div>
+    <video :style="fixStyle" autoplay loop class="fillWidth" src="../components/overview/img/bg.mp4" v-on:canplay="canplay" muted="muted">
+    </video>
+    <img class="title" src="../components/overview/img/title.jpg" alt="">
+<!--    <el-card class="box-card">-->
+<!--      <el-tabs v-model="activeName" stretch="true" @tab-click="handleClick" >-->
+<!--        <el-tab-pane label="登录" name="first" style="font-size: large">-->
+<!--          <div>-->
+<!--            <el-form label-position="top" :model="form" status-icon ref="ruleForm" label-width="100px" class="demo-ruleForm">-->
+<!--              <el-form-item label="用户名">-->
+<!--                <el-input  v-model="form.name"></el-input>-->
+<!--              </el-form-item>-->
+<!--              <el-form-item label="密码" prop="pass">-->
+<!--                <el-input type="password" v-model="form.pass" autocomplete="off"></el-input>-->
+<!--              </el-form-item>-->
+<!--              <el-form-item>-->
+<!--                <el-button type="primary" @click="signin()">登录</el-button>-->
+<!--              </el-form-item>-->
+<!--            </el-form>-->
+<!--          </div>-->
+<!--        </el-tab-pane>-->
+<!--        <el-tab-pane label="注册" name="second">-->
+<!--          <div>-->
+<!--            <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">-->
+<!--              <el-form-item label="用户名">-->
+<!--                <el-input v-model="ruleForm.name"></el-input>-->
+<!--              </el-form-item>-->
+<!--              <el-form-item label="密码" prop="pass">-->
+<!--                <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>-->
+<!--              </el-form-item>-->
+<!--              <el-form-item label="确认密码" prop="checkPass">-->
+<!--                <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>-->
+<!--              </el-form-item>-->
+<!--              <el-form-item>-->
+<!--                <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>-->
+<!--                <el-button @click="resetForm('ruleForm')">重置</el-button>-->
+<!--              </el-form-item>-->
+<!--            </el-form>-->
+<!--          </div>-->
+<!--        </el-tab-pane>-->
+<!--      </el-tabs>-->
+<!--    </el-card>-->
+    <!--      <div class="poster hidden" v-if="!vedioCanPlay">-->
+    <!--        <img :style="fixStyle" src="/Users/Lipyu/Desktop/myGit/fwwb/front_end/fwwb_professional_team/fwwb_front_end/src/components/overview/img/2-2.png" alt="">-->
+    <!--      </div>-->
+<div class="loginPanel" >
+  <div style="width: 350px;margin-left: 50px;margin-left: 65px">
+    <b style="color: white;font-size: 38px;float: left;margin-left: 18px">登录</b>
+    <el-input
+      placeholder="请输入账户"
+      prefix-icon="el-icon-user-solid"
+      v-model="account">
+    </el-input>
+    <el-input
+      placeholder="请输入密码"
+      prefix-icon="el-icon-s-cooperation"
+      v-model="psw"
+      show-password>
+    </el-input>
+    <el-button type="primary">登录</el-button>
+<div>
+  <b style="color: white;font-size: 18px;display: inline-block">服务器状态：</b><b style="color: #2de37f;font-size: 18px;display: inline-block">正常</b>
+  <b style="color: white;font-size: 18px;display: inline-block;margin-left: 100px">延迟：</b><b style="color: white;font-size: 18px;display: inline-block">5ms</b>
+</div>
+  </div>
+  </div>
+    <b class="slogn" style="color: white;font-size: 28px;">生活就像海洋，只有意志坚强的人才能到达彼岸——鲁迅</b>
   </div>
 </template>
 
 <script>
-export default {
-  name: "Welcome",
-  mounted(){
-    //选项卡
-    var item = document.getElementsByClassName("item");
-    var it = item[0].getElementsByTagName("div");
-
-    var content = document.getElementsByClassName("content");
-    var con = content[0].getElementsByTagName("div");
-
-    for(let i=0;i<it.length;i++){
-      it[i].onclick = function(){
-        for(let j=0;j<it.length;j++){
-          it[j].className = '';
-          con[j].style.display = "none";
+  export default {
+    name: 'login',
+    data() {
+      return {
+        account:'',
+        psw:''
+      }
+      var checkAge = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('年龄不能为空'));
         }
-        this.className = "active";
-        it[i].index=i;
-        con[i].style.display = "block";
+        setTimeout(() => {
+          if (!Number.isInteger(value)) {
+            callback(new Error('请输入数字值'));
+          } else {
+            if (value < 18) {
+              callback(new Error('必须年满18岁'));
+            } else {
+              callback();
+            }
+          }
+        }, 1000);
       };
-    }
-
-    if (isPostBack == "False") {
-      GetLastUser();
-    }
-
-    //若已有cookie，自动跳转
-    var userNameValue=getCookieVal("name");
-    var userPassValue=getCookieVal("value")
-    if (userNameValue!=null&&userNameValue!=""&&userPassValue!=null&&userPassValue!=""){
-      window.location.href="http://jwc.bjtu.edu.cn";
-    }
-
-    document.getElementById("login_username_input").onblur=checkAndFillPwd;
-    document.getElementById("login_password_input").onblur=checkLoginPassword;
-    document.getElementById("logon_username_input").onblur=checkLogonUsername;
-    document.getElementById("logon_password_input").onblur=checkLogonPassword;
-    document.getElementById("login_submit").onfocus=login;
-    document.getElementById("logon_submit").onfocus=logon;
-  },
-  methods:{
-          logon() {
-        var obj={
-          username: $("#logon_username_input").val(),
-          password: $("#logon_password_input").val(),
-        };
-        if ((checkLogonUsername()&&checkLogonPassword())){
-
-          //发送请求
-
-
-
-
-        }else if(checkLogonPassword()){
-          alert("用户名格式错误");
-          window.location.href="http://localhost:8080/";
-        }else if (checkLogonUsername()){
-          alert("密码格式错误");
-          window.location.href="http://localhost:8080/";
-        }else {
-          alert("用户名与密码格式错误");
-          window.location.href="http://localhost:8080/";
-        }
-      },
-          login(){
-        //set cookie
-        SetPwdAndChk();
-        var obj={
-          username: $("#login_username_input").val(),
-          password: $("#login_password_input").val(),
-        };
-        if ((checkLoginUsername() && checkLoginPassword())) {
-       //发送请求
-
-        }else if (checkLoginPassword()) {
-          alert("用户名格式错误");
-          window.location.href = "http://localhost:8080/";
-        } else if (checkLoginUsername()) {
-          alert("密码格式错误");
-          window.location.href = "http://localhost:8080/";
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
         } else {
-          alert("用户名与密码格式错误");
-          window.location.href = "http://localhost:8080/";
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
+          }
+          callback();
         }
-      },
-
-      checkAndFillPwd() {
-        //检查格式
-        checkLoginUsername();
-
-        //cookie —— fill in password
-        var usr = document.getElementById('login_username_input').value;
-        var pwd = GetCookie(usr);
-        if (pwd != null) {
-          document.getElementById('remeberyhm').checked = true;
-          document.getElementById('login_password_input').value = pwd;
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.ruleForm.pass) {
+          callback(new Error('两次输入密码不一致!'));
         } else {
-          document.getElementById('remeberyhm').checked = false;
-          document.getElementById('login_password_input').value = "";
+          callback();
         }
+      }
+      return {
+        vedioCanPlay: false,
+        fixStyle: '',
+        activeName: 'first',
+        ruleForm: {
+          name:'',
+          pass: '',
+          checkPass: '',
+          age: ''
+        },
+        form:{
+          name:'',
+          pass:'',
+        },
+        rules: {
+          pass: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          checkPass: [
+            { validator: validatePass2, trigger: 'blur' }
+          ],
+          age: [
+            { validator: checkAge, trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      canplay() {
+        this.vedioCanPlay = true
+      },
+      signin(){
 
       },
-
-      // 校验用户名
-      checkLoginUsername(){
-        // 1.获取用户名的值
-        var username = document.getElementById("login_username_input").value;
-        // 2.定义正则表达式
-        var reg_username = /^\w{6,12}$/;
-        // 3.判断值是否符合正则的规则
-        var flag = reg_username.test(username);
-
-        // 4.提示信息
-        var s_username= document.getElementById("login_username_error");
-        if(flag){
-          // 提示绿色对勾
-          s_username.innerHTML = "<img width='20' height='20' src='images/gouhui.png'/>";
-        }else{
-          // 提示红色用户名有误
-          s_username.innerHTML = "用户名格式有误:长度为6～12位";
-          document.getElementById("login_username_input").focus();
-        }
-
-        return flag;
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
       },
-
-      // 校验密码
-      checkLoginPassword() {
-        // 1.获取用户名的值
-        var password = document.getElementById("login_password_input").value;
-        // 2.定义正则表达式
-        var reg_password = /^\w{6,12}$/;
-        // 3.判断值是否符合正则的规则
-        var flag = reg_password.test(password);
-        // 4.提示信息
-        var s_password = document.getElementById("login_password_error");
-        if (flag) {
-          // 提示绿色对勾
-          s_password.innerHTML = "<img width='20' height='20' src='images/gouhui.png'/>";
-        } else {
-          // 提示红色用户名有误
-          s_password.innerHTML = "密码格式有误:长度为6～12位";
-          document.getElementById("login_password_input").focus();
-        }
-
-        return flag;
-      },
-
-      checkLogonUsername(){
-        // 1.获取用户名的值
-        var username = document.getElementById("logon_username_input").value;
-        // 2.定义正则表达式
-        var reg_username = /^\w{6,12}$/;
-        // 3.判断值是否符合正则的规则
-        var flag = reg_username.test(username);
-
-        // 4.提示信息
-        var s_username= document.getElementById("logon_username_error");
-        if(flag){
-          // 提示绿色对勾
-          s_username.innerHTML = "<img width='20' height='20' src='images/gouhui.png'/>";
-        }else{
-          // 提示红色用户名有误
-          s_username.innerHTML = "用户名格式有误:长度为6～12位";
-          document.getElementById("logon_username_input").focus();
-        }
-        return flag;
-      },
-
-      checkLogonPassword() {
-        // 1.获取用户名的值
-        var password = document.getElementById("logon_password_input").value;
-        // 2.定义正则表达式
-        var reg_password = /^\w{6,12}$/;
-        // 3.判断值是否符合正则的规则
-        var flag = reg_password.test(password);
-        // 4.提示信息
-        var s_password = document.getElementById("logon_password_error");
-        if (flag) {
-          // 提示绿色对勾
-          s_password.innerHTML = "<img width='20' height='20' src='images/gouhui.png'/>";
-        } else {
-          // 提示红色用户名有误
-          s_password.innerHTML = "密码格式有误:长度为6～12位";
-          document.getElementById("logon_password_input").focus();
-        }
-        return flag;
-      },
-
-}
-}
-
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    },
+    // mounted: function() {
+    //   window.onresize = () => {
+    //     const windowWidth = document.body.clientWidth
+    //     const windowHeight = document.body.clientHeight
+    //     const windowAspectRatio = windowHeight / windowWidth
+    //     let videoWidth
+    //     let videoHeight
+    //     if (windowAspectRatio < 0.5625) {
+    //       videoWidth = windowWidth
+    //       videoHeight = videoWidth * 0.5625
+    //       this.fixStyle = {
+    //         height: windowWidth * 0.5625 + 'px',
+    //         width: windowWidth + 'px',
+    //         'margin-bottom': (windowHeight - videoHeight) / 2 + 'px',
+    //         'margin-left': 'initial'
+    //       }
+    //     } else {
+    //       videoHeight = windowHeight
+    //       videoWidth = videoHeight / 0.5625
+    //       this.fixStyle = {
+    //         height: windowHeight + 'px',
+    //         width: windowHeight / 0.5625 + 'px',
+    //         'margin-left': (windowWidth - videoWidth) / 2 + 'px',
+    //         'margin-bottom': 'initial'
+    //       }
+    //     }
+    //   }
+    //   window.onresize()
+    // }
+  }
 </script>
 
-<style scoped>
-#welcome {
-  background: url("../assets/bg/2-2.png") no-repeat center;
-  height: 100%;
-  width: 100%;
-  background-size: cover;
-  position: fixed;
-}
-.card{
-  margin: 10rem auto auto;
-  background-color: rgba(54, 78, 147,0.5);
-  height: 15rem;
-  width: 25rem;
-}
-.word{
-  margin-top: 5rem;
-  color: white;
-}
-* {
-  margin: 0;
-  padding: 0;
-}
-.control{
-  width: 24rem;
-  background: white;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%,-50%);
-  border-radius: 5px;
-}
-.item{
-  width: 24rem;
-  height: 60px;
-  background: #eeeeee;
-}
-.item div{
-  width: 12rem;
-  height: 60px;
-  display: inline-block;
-  color: black;
-  font-size: 18px;
-  text-align: center;
-  line-height: 60px;
-  cursor: pointer;
-}
-.content{
-  width: 100%;
-}
-.content div{
-  margin: 20px 30px;
-  display: none;
-  text-align: left;
-}
-p{
-  color: #4a4a4a;
-  margin-top: 30px;
-  margin-bottom: 6px;
-  font-size: 15px;
-}
-.content input[type="text"], .content input[type="password"]{
-  width: 100%;
-  height: 40px;
-  border-radius: 3px;
-  border: 1px solid #adadad;
-  padding: 0 10px;
-  box-sizing: border-box;
-}
-.content input[type="submit"]{
-  margin-top: 40px;
-  width: 100%;
-  height: 40px;
-  border-radius: 5px;
-  color: white;
-  border: 1px solid #f30606;
-  background: #f30606;
-  cursor: pointer;
-  letter-spacing: 4px;
-  margin-bottom: 40px;
-}
-.active{
-  background: white;
-}
-.item div:hover{
-  background: #f6f6f6;
-}
 
-.button {
-  border-color: cornsilk;
-  background-color: rgba(100, 149, 237, .7);
-  color: aliceblue;
-  border-style: hidden;
-  border-radius: 5px;
-  width: 100px;
-  height: 31px;
-  font-size: 16px;
-}
-.error{
-  color: #f30606;
-}
+<style scoped>
+  .homepage-hero-module,
+
+
+  .video-container .poster img,
+  .video-container video {
+    z-index: 0;
+    position: absolute;
+    height: 100%;
+    width : 100%;
+    top: 0;
+    left: 0;
+
+  }
+
+  .filter {
+    z-index: 1;
+    position: absolute;
+    background: rgba(0, 0, 0, 0.4);
+    height: 100%;
+    width : 100%;
+  }
+
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    padding: 18px 0;
+  }
+
+  .box-card {
+    z-index: 10;
+    width: 480px;
+    position: absolute;
+    top: 40%;
+    left: 35%;
+    font-size: 50px;
+  }
+  .loginPanel{
+    z-index: 10;
+    width: 480px;
+    position: absolute;
+    top: 32%;
+    left: 35%;
+    font-size: 50px;
+    /* @Key 透明度 R G B Alpha（不透明度为50%） */
+    background-color: rgba(4, 12, 40, 0.7);
+    border: 2px solid rgba(114, 130, 208, 0.5);
+
+    /* @Key 圆角矩形 */
+    /*-moz-border-radius: 1em;*/
+    /*-webkit-border-radius: 1em;*/
+    border-radius: 0.2em;
+    padding-bottom: 50px;
+    padding-top: 50px;
+  }
+  .title{
+    z-index: 5;
+    position: absolute;
+    top: 10%;
+    left: 20%;
+  }
+  .demo-ruleForm{
+    font-size: large;
+  }
+  .slogn{
+    z-index: 5;
+    position: absolute;
+    top: 91%;
+    left: 30%;
+  }
+
 </style>
